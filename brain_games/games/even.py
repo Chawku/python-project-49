8e6e034a54a0dc73b0eb017d7afc04ad6dde231a
Chawku <chawku.work@gmail.com>
2024-04-05 22:23:51 +0300

    fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,18 +1,17 @@
-from random import randint
+import random
 
 
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def challenge():
-    random_num = randint(1, 100)
-    flag = is_even(random_num)
-    if flag is True:
+    number = random.randint(1, 100)
+    if is_even(number) is True:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    return correct_answer, random_num
+    return correct_answer, number
 
 
-def is_even(random_num):
-    return True if random_num % 2 == 0 else False
+def is_even(number):
+    return True if number % 2 == 0 else False
